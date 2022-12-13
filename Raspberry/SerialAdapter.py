ab214496e16a8281e12570cf041d6bb5ef7c48e9
pat18-dev <101279292+pat18-dev@users.noreply.github.com>
2022-12-13 13:25:27 -0500

    Initial commit

---
+++ Raspberry/SerialAdapter.py
@@ -0,0 +1,18 @@
+import serial
+import requests
+from datetime import datetime
+
+URL = "http://localhost:5000/send/"
+
+if __name__ == "__main__":
+   ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
+   ser.reset_input_buffer()
+
+   while True:
+      print(b"Initialization starts!\n")
+      if ser.in_waiting > 0:
+         line = ser.readline().decode("utf-8").rstrip()
+         now = datetime.now().strftime("%H:%M")
+         data = {"data": line, "time": now}
+         response = requests.post(URL, data=data)
+         print(response.text)
