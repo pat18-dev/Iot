dbd9c1b2379436fc9684ac92f756f75f90aeed02
Iandanwithmore <33789701+Iandanwithmore@users.noreply.github.com>
2023-01-10 23:05:24 -0500

    error in date maybe socket doesnt work for this

--- Server/app.py
+++ Server/app.py
@@ -1,14 +1,10 @@
 from flask import (
-   flash, 
-   redirect, 
    render_template, 
    request, 
-   url_for,
    Flask,
    render_template,
    session
 )
-from flask_socketio import SocketIO
 import os
 from sys import stderr
 from datetime import datetime
@@ -20,14 +16,13 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 app.config["PORT"] = 5000
 DIR_FILE = os.path.join(os.path.dirname(__file__))
-socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route("/")
 def index():
-   # lpath = os.path.join(DIR_FILE, "file", "1.csv")
-   # ReferenceDbAdapter = DbAdapter(lpath)
-   # datos = ReferenceDbAdapter.get()
+   lpath = os.path.join(DIR_FILE, "file", "1.csv")
+   ReferenceDbAdapter = DbAdapter(lpath)
+   datos = ReferenceDbAdapter.get()
    user = {
             "id": "0",
             "name": "administrator",
@@ -35,24 +30,14 @@ def index():
    }
    # # VALUES IN FORMAT
    # # "id","write_at","value"
-   # values = [float(item[2]) for item in datos if item]
-   # nserial = [idx for idx in range(0,len(values))]
-   # return render_template("index.html", title="index", values=values, nserial=nserial, current_user=user)
-   return render_template("index.html", title="index", current_user=user)
+   values = [float(item[2]) for item in datos if item]
+   nserial = [idx for idx in range(0,len(values))]
+   return render_template("index.html", title="index", values=values, nserial=nserial, current_user=user)
 
-@socketio.on('connect')
-def connected():
-   print('Connected')
-
-@socketio.on('disconnect')
-def disconnected():
-   print('Disconnected')
-
-# @app.route('/send_data', methods=['POST'])
-@socketio.on('send_data')
-def send_data(data):
+@app.route('/send_data', methods=['POST'])
+def send_data():
    params = ["id", "write_at_str", "value"]
-   # data = request.get_json()
+   data = request.get_json()
    for item in params:
       if data.get(item) is None:
          raise KeyError(f"Invalid key {item}")
@@ -64,10 +49,8 @@ def send_data(data):
    lpath = os.path.join(DIR_FILE, "file", str(json_data["sensorid"]) + ".csv")
    ReferenceDbAdapter = DbAdapter(lpath)
    ReferenceDbAdapter.save(ReferenceSensor.get_values())
-   socketio.emit("draw", ReferenceSensor.data, broadcast=True)
    return json_data
 
 
 if __name__ == "__main__":
    app.run(debug=True, host='0.0.0.0')
-   socketio.run(app)
